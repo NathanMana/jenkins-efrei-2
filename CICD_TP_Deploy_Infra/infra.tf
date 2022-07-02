2bef9ea7bfeb07cc11a5ee0b7ad7d5c8be3091bb
Mana <nat.manar@gmail.com>
2022-07-02 22:24:58 +0200

    updatng config

--- CICD_TP_Deploy_Infra/infra.tf
+++ CICD_TP_Deploy_Infra/infra.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "eu-west-3"
+  region = "eu-west-1"
 }
 terraform {
   backend "s3" {
     bucket = "mybucketinfra"
     key    = ".terraform/terraform.tfstate"
-    region = "eu-west-3"
+    region = "eu-west-1"
   }
 }
 
@@ -48,7 +48,7 @@ resource "aws_subnet" "subnet-public-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-3a"
+  availability_zone       = "eu-west-1a"
   tags = {
     Name = "${var.env}-subnet-public-1"
   }
@@ -59,7 +59,7 @@ resource "aws_subnet" "subnet-public-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-3b"
+  availability_zone       = "eu-west-1b"
   tags = {
     Name = "${var.env}-subnet-public-2"
   }
@@ -70,7 +70,7 @@ resource "aws_subnet" "subnet-public-3" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-3c"
+  availability_zone       = "eu-west-1c"
   tags = {
     Name = "${var.env}-subnet-public-3"
   }
@@ -82,7 +82,7 @@ resource "aws_subnet" "subnet-private-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-3a"
+  availability_zone       = "eu-west-1a"
   tags = {
     Name = "${var.env}-subnet-private-1"
   }
@@ -93,7 +93,7 @@ resource "aws_subnet" "subnet-private-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-3b"
+  availability_zone       = "eu-west-1b"
   tags = {
     Name = "${var.env}-subnet-private-2"
   }
@@ -104,7 +104,7 @@ resource "aws_subnet" "subnet-private-3" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-3c"
+  availability_zone       = "eu-west-1c"
   tags = {
     Name = "${var.env}-subnet-private-3"
   }
